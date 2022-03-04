e8cb5ca0059d8d69301a4feb54f61789390e8b70
AdriContador <acontador2@institutmvm.cat>
2022-03-04 08:53:28 +0100

    Initial commit

---
+++ literals.py
@@ -0,0 +1,10 @@
+MSG1 = 'Introdueix el teu nom: '
+MSG2 = 'Introdueix el teu cognom: '
+MSG3 = 'Introdueix la teva edat: '
+MSG4 = 'En quina tecnologia estàs interessat?' \
+       '   1. Realitat virtual' \
+       '   2. Realitat augmentada        '
+MSG5 = 'Realitat virtual'
+MSG6 = 'Realitat augmentada'
+MSG7 = 'No hi ha tecnologia assignada.'
+MSG8 = 'Introdueix un 0 per terminar la introducció de registres, o qualsevol altre número per continuar introduint.'
